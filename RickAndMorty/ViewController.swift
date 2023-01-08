b4dc8859dcffb4d1bcdf169d5a766af07e73964a
EMRE KOCAK <m.emrekocakk@gmail.com>
2023-01-08 16:50:12 +0300

    Initial commit

---
+++ RickAndMorty/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RickAndMorty
+//
+//  Created by Emre Kocak on 8.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+      
+        view.backgroundColor = .red
+    }
+
+
+}
+
